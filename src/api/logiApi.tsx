2680656ee8cff318e00f476e7454961c76c51416
chetchet1 <chetchet@nate.com>
2024-09-22 06:33:54 +0900

    try to jenkins

--- src/api/logiApi.tsx
+++ src/api/logiApi.tsx
@@ -1,8 +1,5 @@
 import axios from 'axios';
 
-const baseURL = process.env.REACT_APP_DOCKER_API_URL;
-console.log(baseURL); // 환경변수 로그 출력
-
 export default axios.create({
-  baseURL: baseURL
+  baseURL: `${process.env.REACT_APP_DOCKER_API_URL}`
 });
